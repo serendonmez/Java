5dd6caa21437390ac6fea74288487f36a4983796
serendonmez <serendnmez@gmail.com>
2024-04-20 00:24:04 +0200

    20.04.24

---
+++ src/Day40_Abstraction/C01_kuralciOlmayanParent.java
@@ -0,0 +1,21 @@
+package Day40_Abstraction;
+
+public class C01_kuralciOlmayanParent {
+    public void method1 (){
+
+
+
+
+    }
+
+    public void method2(){
+
+
+
+    }
+
+    public void method3(){
+
+
+    }
+}
---
+++ src/Day40_Abstraction/C02_KralChild.java
@@ -0,0 +1,32 @@
+package Day40_Abstraction;
+
+public class C02_KralChild extends C01_kuralciOlmayanParent{
+
+
+ /*
+        Inheritance'da kral child class'dir
+        Parent class'daki hic bir method'u kendine uyarlamak ZORUNDA DEGILDIR
+
+        istedigi method'u parent class'dan kullanabilir
+        istedigi method'u override ederek kendisine uyarlayabilir
+        istedigi bir method parent class'da olmasa da child class kendisi olusturabilir
+
+        parent class bu secimlerde HICBIR SOZ soylemez
+
+        OOP'nin 4.prensibi olan abstraction
+        parent class'lari inheritance konusunda soz sahibi yapabilmek icin kullanilir
+
+     */
+
+
+
+
+
+
+
+}
+
+
+
+
+
